053b03598d8150e30b4d8cbad70da9ad51fe4ace
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-02-29 12:11:11 +0100

    Added documentation to `get_functions()`

--- R/get_functions.R
+++ R/get_functions.R
@@ -1,3 +1,9 @@
+#' @title Get functions from the global environment
+#' @description Retrieves functions defined in the global environment and
+#' returns them to the target environment.
+#' @param target_env The environment to which the functions will be returned.
+#' @return All functions from the global environment.
+#' @author Waldir Leoncio
 get_functions <- function(target_env) {
   functions <- list()
   functions_list <- ls(envir = .GlobalEnv)
